919555aa801957256102bb3220b388d80e24377d
abhik26173 <37187408+abhik26173@users.noreply.github.com>
2025-05-18 05:14:39 +0530

    Next code for CURD operation
    CURD operations

--- job-board/job-board/server/server.js
+++ job-board/job-board/server/server.js
@@ -5,20 +5,28 @@ import express from 'express';
 import { readFile } from 'node:fs/promises';
 import { authMiddleware, handleLogin } from './auth.js';
 import { resolvers } from './resolvers.js';
+import { getUser } from './db/users.js';
 
 const PORT = 9000;
 
 const app = express();
-
-app.use(cors(), express.json(), authMiddleware); // Middleware for authentication and cross-origin resource sharing
+app.use(cors(), express.json(), authMiddleware);
 
 app.post('/login', handleLogin);
 
-const typeDefs = await readFile('./schema.graphql', 'utf8'); // Load GraphQL schema from file
+const typeDefs = await readFile('./schema.graphql', 'utf8');
 
-const apolloServer = new ApolloServer({ typeDefs, resolvers }); // Create Apollo Server instance with schema and resolvers
+async function getContext({ req }) {
+  if (req.auth) {
+    const user = await getUser(req.auth.sub);
+    return { user };
+  }
+  return {};
+}
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
 await apolloServer.start();
-app.use('/graphql', apolloMiddleware(apolloServer)); // Middleware for handling GraphQL requests
+app.use('/graphql', apolloMiddleware(apolloServer, { context: getContext }));
 
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
